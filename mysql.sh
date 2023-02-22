e1dcc614d9ed37ef1433765ce93aab601df54d84
Harika Ramidi <ramidi.h@northeastern.edu>
2023-02-22 11:17:36 -0500

    sudo sed -i 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf

--- mysql.sh
+++ mysql.sh
@@ -17,9 +17,9 @@
 EOD
       sudo sed -i 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf
       sudo sed -i 's/peer/md5/g' /var/lib/pgsql/data/pg_hba.conf
-      sudo echo "host    all             all             0.0.0.0/0               md5" >> /var/lib/pgsql/data/pg_hba.conf
-      sudo echo "listen_addresses = '*'" >> /var/lib/pgsql/data/postgresql.conf
-      
+      sudo echo '"host    all             all             0.0.0.0/0               md5" >> /var/lib/pgsql/data/pg_hba.conf'
+      sudo echo '"listen_addresses = '*'" >> /var/lib/pgsql/data/postgresql.conf'
+
       curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
       . ~/.nvm/nvm.sh && nvm install 16
       node -e "console.log('Running Node.js ' + process.version)"
