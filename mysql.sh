a17d8f2b58a95aca82757ebd8412e243f46d47c8
Harika Ramidi <ramidi.h@northeastern.edu>
2023-02-22 13:17:54 -0500

    authentication type changes

--- mysql.sh
+++ mysql.sh
@@ -18,6 +18,7 @@ EOD
 
       sudo sed -i 's/peer/md5/g' /var/lib/pgsql/data/pg_hba.conf
       sudo sed -i 's/md5/trust/g' /var/lib/pgsql/data/pg_hba.conf
+      sudo sed -i 's/ident/trust/g' /var/lib/pgsql/data/pg_hba.conf
       echo "host    all             all             0.0.0.0/0               trust" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
       echo "listen_addresses = '*'" | sudo tee -a /var/lib/pgsql/data/postgresql.conf
 
