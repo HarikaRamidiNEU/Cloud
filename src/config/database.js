9ba3a2d5a18d8568568e28cb807930f87606843f
Harika Ramidi <ramidi.h@northeastern.edu>
2023-02-22 12:46:32 -0500

    postgres without password authentication

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import dotenv  from "dotenv"
 dotenv.config();
 
 import { Sequelize } from "sequelize";
-const sequelize = new Sequelize('postgres://'+(process.env.DATABASE_USER || "postgres")+':'+(process.env.DATABASE_Password || "XoXo1998")+'@'+(process.env.DATABASE_HOST || "localhost")+':'+(process.env.DATABASE_PORT || "5432")+'/'+(process.env.DATABASE_NAME || "postgres"))
+const sequelize = new Sequelize('postgres://'+(process.env.DATABASE_USER || "postgres")+'@'+(process.env.DATABASE_HOST || "localhost")+':'+(process.env.DATABASE_PORT || "5432")+'/'+(process.env.DATABASE_NAME || "postgres"))
 sequelize.authenticate().then(() => {
   console.log('Connection has been established successfully.');
 }).catch(err => {
