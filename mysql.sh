71d482ae4cc77542280e1b353b4bf7a5f9293541
Harika Ramidi <ramidi.h@northeastern.edu>
2023-02-21 20:29:45 -0500

    updated shell script and packer

--- mysql.sh
+++ mysql.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-      sudo apt -y update
-      sudo apt -y upgrade
+      sudo yum update -y
+      sudo yum -y upgrade
       curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
       . ~/.nvm/nvm.sh
       nvm install node
       
-      sudo amazon-linux-extras install postgresql15
+      sudo amazon-linux-extras enable postgresql14
+      sudo yum install postgresql-server -y
       tar -xf /home/ec2user/app.tar.gz
       sudo mv /tmp/appservice.service /etc/systemd/system/appservice.service
+
+      sudo postgresql-setup initdb
+      sudo systemctl start postgresql
+      sudo systemctl enable postgresql
+      sudo -u postgres psql
+      ALTER USER postgres PASSWORD 'password';
 
       sudo systemctl daemon-reload
       sudo systemctl enable appservice.service
