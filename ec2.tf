9e2646acaddd684431385aeb8cb2f55ba170b738
Harika Ramidi <ramidi.h@northeastern.edu>
2023-02-22 02:04:38 -0500

    ec2 instance creation with securityGroup

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,10 @@
+
 resource "aws_instance" "application-ec2" {
-  ami = var.my_ami
+  ami           = var.my_ami
   instance_type = var.ec2_instance_type
-  key_name = var.aws_key_pair
-  security_groups = ["application"]
+  key_name      = var.aws_key_pair
+  subnet_id     = aws_subnet.public_subnets[0].id
+  vpc_security_group_ids = [
+    aws_security_group.application.id
+  ]
 }
