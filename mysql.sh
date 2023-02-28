042d9e7812bd2b4464e5c5d307d8924818e74096
Harika Ramidi <ramidi.h@northeastern.edu>
2023-02-27 22:11:39 -0500

    postgres update in shell script

--- mysql.sh
+++ mysql.sh
@@ -2,27 +2,27 @@
 
       sudo yum update -y
 
-      sudo amazon-linux-extras install postgresql9.6 -y
-      sudo yum install postgresql postgresql-server java-1.8.0 glibc.i686 -y
-      sudo /usr/bin/postgresql-setup --initdb --unit postgresql
-      sudo systemctl start postgresql
-      sudo systemctl enable --now postgresql
-      sudo systemctl status postgresql
-      username="postgres"
-      password="XoXo1998"
+#       sudo amazon-linux-extras install postgresql9.6 -y
+#       sudo yum install postgresql postgresql-server java-1.8.0 glibc.i686 -y
+#       sudo /usr/bin/postgresql-setup --initdb --unit postgresql
+#       sudo systemctl start postgresql
+#       sudo systemctl enable --now postgresql
+#       sudo systemctl status postgresql
+#       username="postgres"
+#       password="XoXo1998"
 
-      sudo passwd ${username} << EOD
-      ${password}
-      ${password}
-EOD
+#       sudo passwd ${username} << EOD
+#       ${password}
+#       ${password}
+# EOD
 
-      sudo sed -i 's/peer/md5/g' /var/lib/pgsql/data/pg_hba.conf
-      sudo sed -i 's/md5/trust/g' /var/lib/pgsql/data/pg_hba.conf
-      sudo sed -i 's/ident/trust/g' /var/lib/pgsql/data/pg_hba.conf
-      echo "host    all             all             0.0.0.0/0               trust" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
-      echo "listen_addresses = '*'" | sudo tee -a /var/lib/pgsql/data/postgresql.conf
+#       sudo sed -i 's/peer/md5/g' /var/lib/pgsql/data/pg_hba.conf
+#       sudo sed -i 's/md5/trust/g' /var/lib/pgsql/data/pg_hba.conf
+#       sudo sed -i 's/ident/trust/g' /var/lib/pgsql/data/pg_hba.conf
+#       echo "host    all             all             0.0.0.0/0               trust" | sudo tee -a /var/lib/pgsql/data/pg_hba.conf
+#       echo "listen_addresses = '*'" | sudo tee -a /var/lib/pgsql/data/postgresql.conf
 
-      sudo systemctl restart postgresql
+#       sudo systemctl restart postgresql
 
       sudo yum install gcc-c++ make -y
       curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
