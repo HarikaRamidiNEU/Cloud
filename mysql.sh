efc12dd285ebbdd684e279070f8b2ffc2b34e21d
Harika Ramidi <ramidi.h@northeastern.edu>
2023-02-28 12:52:45 -0500

    removing autostart

--- mysql.sh
+++ mysql.sh
@@ -28,14 +28,14 @@
       curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
       sudo yum update -y
       sudo yum install nodejs -y
-      sudo cp /tmp/app.tar.gz /home/ec2-user/
-      sudo tar -xzf /home/ec2-user/app.tar.gz -C /home/ec2-user/
-      sudo chmod -R +x /home/ec2-user/
-      sudo /usr/bin/npm --prefix /home/ec2-user/ install /home/ec2-user/
-      sudo chmod -R +x /home/ec2-user/
+      # sudo cp /tmp/app.tar.gz /home/ec2-user/
+      # sudo tar -xzf /home/ec2-user/app.tar.gz -C /home/ec2-user/
+      # sudo chmod -R +x /home/ec2-user/
+      # sudo /usr/bin/npm --prefix /home/ec2-user/ install /home/ec2-user/
+      # sudo chmod -R +x /home/ec2-user/
      
-      sudo mv /tmp/appservice.service /etc/systemd/system/appservice.service
-      sudo systemctl daemon-reload
-      sudo systemctl enable appservice.service
-      sudo systemctl start appservice.service
-      sudo systemctl status appservice.service
+      # sudo mv /tmp/appservice.service /etc/systemd/system/appservice.service
+      # sudo systemctl daemon-reload
+      # sudo systemctl enable appservice.service
+      # sudo systemctl start appservice.service
+      # sudo systemctl status appservice.service
