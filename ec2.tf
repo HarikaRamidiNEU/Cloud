8e19f3cd2153a373583c622c1f13c44daf6a5c9a
Harika Ramidi <ramidi.h@northeastern.edu>
2023-02-27 22:02:39 -0500

    database rds and ec2 with userdata

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "application-ec2" {
   instance_type           = var.ec2_instance_type
   key_name                = var.aws_key_pair
   subnet_id               = aws_subnet.public_subnets[0].id
+  iam_instance_profile    = aws_iam_instance_profile.ec2_profile.name
   disable_api_termination = false
   vpc_security_group_ids = [
     aws_security_group.application.id
@@ -11,7 +12,15 @@ resource "aws_instance" "application-ec2" {
   tags = {
     Name = "Application EC2"
   }
-
+  user_data = <<EOF
+#!/bin/bash
+export DATABASE_USER=csye6225
+export DATABASE_HOST=${aws_db_instance.databaseInstance.endpoint}
+export DATABASE_NAME=csye6225
+export DATABASE_Password=password
+export DATABASE_PORT=5432
+export AWS_BUCKET_NAME=${local.bucket_name}
+EOF
   depends_on = [
     aws_subnet.public_subnets,
   ]
@@ -24,3 +33,4 @@ resource "aws_eip" "lb" {
     aws_instance.application-ec2,
   ]
 }
+
