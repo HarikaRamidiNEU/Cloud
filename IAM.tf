04da0a16feac3ab40c9d8e775268866392937d6f
Harika Ramidi <ramidi.h@northeastern.edu>
2023-04-05 14:29:38 -0400

    load balancing and auto scaling

--- IAM.tf
+++ IAM.tf
@@ -22,6 +22,55 @@ resource "aws_iam_policy" "WebAppS3" {
     ]
   })
 }
+
+resource "aws_iam_policy" "EC2-lb" {
+  name        = "EC2-load_balancer"
+  path        = "/"
+  description = "My webapp elb policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:RunInstances",
+          "ec2:DescribeInstances",
+          "ec2:StopInstances",
+          "ec2:TerminateInstances",
+          "ec2:AttachInstances",
+          "ec2:DetachInstances",
+          "autoscaling:CreateAutoScalingGroup",
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:UpdateAutoScalingGroup",
+          "autoscaling:CreateLaunchConfiguration",
+          "autoscaling:DescribeLaunchConfigurations",
+          "autoscaling:CreateLaunchTemplate",
+          "autoscaling:DescribeLaunchTemplates",
+          "autoscaling:PutScalingPolicy",
+          "autoscaling:DescribeScalingPolicies",
+          "autoscaling:DeleteAutoScalingGroup",
+          "autoscaling:DeleteLaunchConfiguration",
+          "autoscaling:DeleteLaunchTemplate",
+          "autoscaling:DeletePolicy",
+          "cloudwatch:PutMetricData",
+          "cloudwatch:GetMetricData",
+          "cloudwatch:DescribeAlarms",
+          "cloudwatch:DeleteAlarms",
+          "ec2:DescribeTags",
+          "ec2:DescribeInstanceAttribute",
+          "ec2:DescribeInstances",
+          "ec2:DescribeRegions",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeRouteTables"
+        ]
+        Effect = "Allow"
+        "Resource" : "*"
+    }]
+  })
+
+}
 resource "aws_iam_role" "EC2-CSYE6225" {
   name = "EC2-CSYE6225"
 
@@ -54,6 +103,12 @@ resource "aws_iam_policy_attachment" "cloudwatch_policy_role" {
   name       = "cloudWatchattachment"
   roles      = [aws_iam_role.EC2-CSYE6225.name]
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_policy_attachment" "elb_policy_role" {
+  name       = "cloudWatchattachment"
+  roles      = [aws_iam_role.EC2-CSYE6225.name]
+  policy_arn = aws_iam_policy.EC2-lb.arn
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
--- securityGroup.tf
+++ securityGroup.tf
@@ -1,35 +1,6 @@
 resource "aws_security_group" "application" {
   name   = "application"
   vpc_id = aws_vpc.main.id
-  #Incoming traffic
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 5432
     to_port     = 5432
@@ -71,4 +42,52 @@ resource "aws_security_group" "database" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_security_group" "loadbalancer" {
+  name   = "loadbalancer"
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "load balancer Security Group"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "ec2ELBConnection1" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.loadbalancer.id
+
+  security_group_id = aws_security_group.application.id
+}
+
+resource "aws_security_group_rule" "ec2ELBConnection2" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.loadbalancer.id
+
+  security_group_id = aws_security_group.application.id
 }
