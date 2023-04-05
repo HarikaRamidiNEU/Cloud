907ec0a4f424579b2119e298564aeece7e7c1bf6
Harika Ramidi <ramidi.h@northeastern.edu>
2023-04-05 16:59:55 -0400

    load balancing changes

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,8 +1,5 @@
-resource "aws_launch_configuration" "asg_launch_config" {
-  image_id      = var.my_ami
-  instance_type = "t2.micro"
-  key_name      = var.aws_key_pair
-  user_data     = <<EOF
+data "template_file" "user_data" {
+  template = <<EOF
 #!/bin/bash
 yum update -y
 cd /home/ec2-user/src/
@@ -18,15 +15,24 @@ cp .env /home/ec2-user/
 chmod -R 777 /home/ec2-user/
 
 EOF
-
-  associate_public_ip_address = true
-  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
-  security_groups             = [aws_security_group.application.id]
+}
+resource "aws_launch_template" "asg_launch_config" {
+  image_id      = var.my_ami
+  instance_type = "t2.micro"
+  key_name      = var.aws_key_pair
+  user_data     = base64encode(data.template_file.user_data.rendered)
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_profile.name
+  }
+  vpc_security_group_ids = [aws_security_group.application.id]
 }
 
 resource "aws_autoscaling_group" "asg" {
-  name                      = "myASG"
-  launch_configuration      = aws_launch_configuration.asg_launch_config.id
+  name = "myASG"
+  launch_template {
+    id      = aws_launch_template.asg_launch_config.id
+    version = "$Latest"
+  }
   vpc_zone_identifier       = [aws_subnet.public_subnets[0].id, aws_subnet.public_subnets[1].id, aws_subnet.public_subnets[2].id]
   min_size                  = 1
   max_size                  = 3
@@ -49,23 +55,8 @@ resource "aws_autoscaling_group" "asg" {
 resource "aws_autoscaling_policy" "scale_up" {
   name                   = "scale-up-policy"
   autoscaling_group_name = aws_autoscaling_group.asg.name
-
-  policy_type = "TargetTrackingScaling"
-
-  target_tracking_configuration {
-    predefined_metric_specification {
-      predefined_metric_type = "ASGAverageCPUUtilization"
-    }
-    target_value = 5.0
-  }
-}
-
-
-resource "aws_autoscaling_policy" "scale_down" {
-  name                   = "scale-down-policy"
-  autoscaling_group_name = aws_autoscaling_group.asg.name
-
-  policy_type = "TargetTrackingScaling"
+  adjustment_type        = "ChangeInCapacity"
+  policy_type            = "TargetTrackingScaling"
 
   target_tracking_configuration {
     predefined_metric_specification {
@@ -107,33 +98,7 @@ resource "aws_lb_listener" "my_listener" {
   }
 }
 
-resource "aws_lb_listener" "my_listener1" {
-  load_balancer_arn = aws_lb.application_lb.arn
-  port              = 443
-  protocol          = "HTTPS"
-
-  default_action {
-    target_group_arn = aws_lb_target_group.target_group.arn
-    type             = "forward"
-  }
-}
-
-# Attach Target Group to Auto Scaling Group
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.asg.name
-  alb_target_group_arn   = aws_lb_target_group.target_group.arn
-}
-
-data "aws_instance" "ec2_instance_ids" {
-  filter {
-    name   = "tag:Name"
-    values = ["web-app"]
-  }
-}
-
-# Register Auto Scaling Group instances with Target Group
-resource "aws_lb_target_group_attachment" "target_group_attachment" {
-  target_group_arn = aws_lb_target_group.target_group.arn
-  target_id        = data.aws_instance.ec2_instance_ids.id
-  port             = "8080"
+  lb_target_group_arn    = aws_lb_target_group.target_group.arn
 }
